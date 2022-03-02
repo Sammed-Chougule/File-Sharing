38dcacc26985409be18e9cb524a87b3f431ae541
sammed <sammedsanjaychougule@gmail.com>
2022-03-02 13:54:50 +0530

    Better error handling

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "/config/.env") });
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3008;
 
 const connectDB = require("./utils/db");
 const logger = require("./logger/createLogger");
 
-connectDB();
+connectDB().catch((error) => {
+  logger.error(error);
+});
 
 app.use("/api/files", require("./routes/userRoutes"));
 
