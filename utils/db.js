597f730f03eb9de11dcba94b76928af8b6a41400
sammed <sammedsanjaychougule@gmail.com>
2022-03-02 13:54:15 +0530

    Used try and catch

--- utils/db.js
+++ utils/db.js
@@ -1,20 +1,18 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 
 const logger = require("../logger/createLogger");
 
-function connectDB() {
-  mongoose
-    .connect(process.env.MONGO_CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      logger.info("MongoDB Connected");
-    })
-    .catch((err) => {
-      logger.info("Connection Failed ", err);
-    });
-}
+const connectDB = async () => {
+  try {
+    const con = await mongoose
+      .connect(process.env.MONGO_CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+    logger.info("connected", con);
+  } catch (err) {
+    throw new Error("connection failed");
+  }
+};
 
 module.exports = connectDB;
