14d80539ef3a20fdabb18637de2b7fc219d65884
Sammed-Chogule <sammedsanjaychogule@gmail.com>
2022-03-17 12:48:45 +0530

    Responder changes

--- middleware/userValidationRegex.js
+++ middleware/userValidationRegex.js
@@ -1,13 +1,5 @@
 /* eslint-disable consistent-return */
-const {
-  nameFormatErr,
-  confPasswordFormatErr,
-  passwordFormatErr,
-  emailFormatErr,
-  userNameFormatErr,
-  dobFormatErr,
-  genderFormatErr,
-} = require("../utils/responder");
+const { responder } = require("../utils/responder");
 
 const regex = (req, res, next) => {
   try {
@@ -19,27 +11,27 @@ const regex = (req, res, next) => {
     const password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
 
     if (!name.test(req.body.name)) {
-      return nameFormatErr(res);
+      return responder(400, "Write name in correct format", res);
     }
 
     if (!gender.test(req.body.gender)) {
-      return genderFormatErr(res);
+      return responder(400, "Write gender in correct format", res);
     }
 
     if (!dob.test(req.body.dob)) {
-      return dobFormatErr(res);
+      return responder(400, "Write dob in correct format", res);
     }
     if (!userName.test(req.body.userName)) {
-      return userNameFormatErr(res);
+      return responder(400, "Write userName in correct format", res);
     }
     if (!email.test(req.body.email)) {
-      return emailFormatErr(res);
+      return responder(400, "Write email in correct format", res);
     }
     if (!password.test(req.body.password)) {
-      return passwordFormatErr(res);
+      return responder(400, "Write password in correct format", res);
     }
     if (req.body.password !== req.body.confirmPassword) {
-      return confPasswordFormatErr(res);
+      return responder(400, "Password doesn't match", res);
     }
     next();
   } catch (error) {
